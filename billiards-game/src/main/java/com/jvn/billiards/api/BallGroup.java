2d3caf25e158a26a12744143068a947e94728c3b
i6385148 <mehmetogluilgaz07@gmail.com>
2025-11-02 19:42:35 +0100

    refactor: remove deprecated game demo

--- billiards-game/src/main/java/com/jvn/billiards/api/BallGroup.java
+++
@@ -1,9 +0,0 @@
-package com.jvn.billiards.api;
-
-public enum BallGroup {
-  CUE,
-  EIGHT,
-  SOLID,
-  STRIPE,
-  UNKNOWN
-}
--- billiards-game/src/main/java/com/jvn/billiards/api/BilliardsGameListener.java
+++
@@ -1,9 +0,0 @@
-package com.jvn.billiards.api;
-
-public interface BilliardsGameListener {
-  void onShotBegan(int playerIndex);
-  void onBallPocketed(int playerIndex, int ballId);
-  void onShotEnded(int playerIndex, ShotOutcome outcome, ShotStats stats);
-  void onTurnChanged(int playerIndex);
-  void onGameOver(int winningPlayerIndex);
-}
